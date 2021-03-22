4a61d74ac4fd41de7acad6f53ad72c497314f801
Estevan Hammer <ejf@utexas.edu>
2021-03-22 10:03:21 -0700

    Update flask integration

---
+++ templates/__react_static_root__/src/pages/404.576f2b41.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{134:function(e,n,t){"use strict";t.r(n);var a=t(0),l=t.n(a),r=t(93);n.default=function(e){return l.a.createElement("div",null,l.a.createElement(r.a,null," "),l.a.createElement("h1",null,JSON.stringify(e)))}}}]);
