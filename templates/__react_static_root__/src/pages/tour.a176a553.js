0191e885af5f2465e56727ed299bb5ae75deb0e8
Estevan Hammer <ejf@utexas.edu>
2021-03-22 10:15:23 -0700

    Update dist changes

---
+++ templates/__react_static_root__/src/pages/tour.a176a553.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{134:function(e,n,t){"use strict";t.r(n);var a=t(0),l=t.n(a),u=t(92);n.default=function(e){return l.a.createElement("div",null,l.a.createElement(u.a,null," "),l.a.createElement("h1",null,"Tours"))}}}]);
