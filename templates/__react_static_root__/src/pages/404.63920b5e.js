de74d6bc513047667eeea1325ee2e78e608dfbab
Estevan Hammer <ejf@utexas.edu>
2021-03-22 12:15:28 -0700

    update the dist

--- templates/__react_static_root__/src/pages/404.63920b5e.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{131:function(e,n,t){"use strict";t.r(n);var a=t(0),l=t.n(a),r=t(92);n.default=function(e){return l.a.createElement("div",null,l.a.createElement(r.a,null," "),l.a.createElement("h1",null,JSON.stringify(e)))}}}]);
